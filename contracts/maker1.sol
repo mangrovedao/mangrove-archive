776768b8aa61d20f477a3b6db05e4808ec7cb9f4
jean.krivine@pps.univ-paris-diderot.fr <jean.krivine@irif.fr>
2020-09-07 15:23:13 +0200

    Allowing access to newOrder() function from the Maker contract

--- contracts/maker1.sol
+++ contracts/maker1.sol
@@ -2,31 +2,39 @@
 pragma solidity ^0.7.0;
 
 interface ERC20 {
-  function transferFrom(
-    address,
-    address,
-    uint256
-  ) external returns (bool);
-
-  function approve(address, uint256) external returns (bool);
+  function approve(address dexAddr, uint256 amount) external returns (bool);
 }
 
 interface Dex {
   function REQ_TOKEN() external returns (address);
 
   function OFR_TOKEN() external returns (address);
+
+  function newOrder(
+    uint256 makerWants,
+    uint256 makerGives,
+    uint256 gasWanted,
+    uint256 pivotId
+  ) external;
+
+  function balanceOf(address maker) external returns (uint256);
+
+  function best() external returns (uint256);
+
+  function penaltyPerGas() external returns (uint256);
 }
 
 contract Maker {
   address immutable REQ_TOKEN;
   address immutable OFR_TOKEN;
-  address immutable DEX; // Address of a (REQ_TOKEN,OFR_TOKEN) DEX
+  address payable immutable DEX; // Address of a (REQ_TOKEN,OFR_TOKEN) DEX
   address private immutable THIS;
+  uint32 private execGas;
 
   constructor(
     address reqToken,
     address ofrToken,
-    address dex2
+    address payable dex2
   ) {
     require(Dex(dex2).REQ_TOKEN() == reqToken);
     require(Dex(dex2).OFR_TOKEN() == ofrToken);
