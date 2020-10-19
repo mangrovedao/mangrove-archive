1d4d5e4776b3f4efb9f5f82eb67de5113a111ee6
Jean Krivine <jean.krivine@irif.fr>
2020-10-19 11:00:12 +0200

    Adding functions to test order book structure

--- contracts/Dex_Test.sol
+++ contracts/Dex_Test.sol
@@ -17,39 +17,24 @@ import "@nomiclabs/buidler/console.sol";
 // Otherwise bytecode can be too large. See EIP 170 for more on size limit:
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-170.md
 
-contract Dex_Test_Pre_Pre_Pre_Pre {
-  function setup() public returns (TestToken, TestToken) {
-    TestToken aToken;
-    TestToken bToken;
-    aToken = new TestToken(address(this), "A", "$A");
-    bToken = new TestToken(address(this), "B", "$B");
-    return (aToken, bToken);
+// Pretest libraries are for deploying large contracts independently.
+// Otherwise bytecode can be too large. See EIP 170 for more on size limit:
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-170.md
+library DexPre0 {
+  function setup() external returns (TestToken, TestToken) {
+    return (
+      new TestToken(address(this), "A", "$A"),
+      new TestToken(address(this), "B", "$B")
+    );
   }
 }
 
-contract Dex_Test_Pre_Pre_Pre {
-  Dex_Test_Pre_Pre_Pre_Pre pre4;
-
-  constructor(Dex_Test_Pre_Pre_Pre_Pre _pre4) {
-    pre4 = _pre4;
-  }
-
-  function setup()
-    public
-    returns (
-      TestToken,
-      TestToken,
-      Dex
-    )
+library DexPre1 {
+  function setup(TestToken aToken, TestToken bToken)
+    external
+    returns (Dex dex)
   {
-    DexDeployer deployer;
-    TestToken aToken;
-    TestToken bToken;
-
-    (aToken, bToken) = pre4.setup();
-
-    deployer = new DexDeployer(msg.sender);
-
+    DexDeployer deployer = new DexDeployer(address(this));
     deployer.deploy({
       initialDustPerGasWanted: 100,
       initialMinFinishGas: 30000,
@@ -58,64 +43,23 @@ contract Dex_Test_Pre_Pre_Pre {
       ofrToken: aToken,
       reqToken: bToken
     });
-
-    return (aToken, bToken, deployer.dexes(aToken, bToken));
+    return deployer.dexes(aToken, bToken);
   }
 }
 
-contract Dex_Test_Pre_Pre {
-  Dex_Test_Pre_Pre_Pre pre3;
-  TestToken aToken;
-  TestToken bToken;
-  Dex dex;
-
-  constructor(Dex_Test_Pre_Pre_Pre _pre3) {
-    pre3 = _pre3;
-  }
-
-  function setup()
-    public
-    returns (
-      TestToken,
-      TestToken,
-      Dex,
-      TestTaker
-    )
-  {
-    (aToken, bToken, dex) = pre3.setup();
-    return (aToken, bToken, dex, new TestTaker(dex));
+library DexPre2 {
+  function setup(Dex dex) external returns (TestMaker, TestMoriartyMaker) {
+    return (new TestMaker(dex), new TestMoriartyMaker(dex));
   }
 }
 
-contract Dex_Test_Pre {
-  Dex_Test_Pre_Pre pre2;
-
-  constructor(Dex_Test_Pre_Pre _pre2) {
-    pre2 = _pre2;
-  }
-
-  function setup()
-    public
-    returns (
-      TestToken,
-      TestToken,
-      Dex,
-      TestTaker,
-      TestMaker,
-      TestMoriartyMaker
-    )
-  {
-    (TestToken aToken, TestToken bToken, Dex dex, TestTaker taker) = pre2
-      .setup();
-    TestMaker maker;
-    maker = new TestMaker(dex);
-    TestMoriartyMaker evilMaker;
-    evilMaker = new TestMoriartyMaker(dex);
-    return (aToken, bToken, dex, taker, maker, evilMaker);
+library DexPre3 {
+  function setup(Dex dex) external returns (TestTaker) {
+    return new TestTaker(dex);
   }
 }
 
-contract Dex_Test is Test, Display {
+contract Dex_Test is Test {
   Dex dex;
   TestMoriartyMaker evilMaker;
   TestMaker maker;
@@ -123,8 +67,85 @@ contract Dex_Test is Test, Display {
   TestToken aToken;
   TestToken bToken;
 
-  constructor(Dex_Test_Pre pre) {
-    (aToken, bToken, dex, taker, maker, evilMaker) = pre.setup();
+  function a_beforeAll() public {
+    //console.log("IN BEFORE ALL");
+    (aToken, bToken) = DexPre0.setup();
+  }
+
+  function b_beforeAll() public {
+    dex = DexPre1.setup(aToken, bToken);
+  }
+
+  function c_beforeAll() public {
+    (maker, evilMaker) = DexPre2.setup(dex);
+    taker = DexPre3.setup(dex);
+    address(maker).transfer(50 ether);
+    maker.provisionDex(10 ether);
+    address(evilMaker).transfer(50 ether);
+    evilMaker.provisionDex(10 ether);
+
+    aToken.mint(address(maker), 5 ether);
+    bToken.mint(address(taker), 5 ether);
+    maker.approve(aToken, 5 ether);
+    evilMaker.approve(aToken, 5 ether);
+    taker.approve(bToken, 5 ether);
+  }
+
+  // orderList[i] starts with the orderId at position i, followed by order info
+  // minus next field.
+  struct Spec {
+    uint orderId;
+    uint wants;
+    uint gives;
+    uint gasWanted;
+    uint minFinishGas;
+    uint penaltyPerGas;
+    address maker;
+  }
+
+  function specOf(
+    uint orderId,
+    uint wants,
+    uint gives,
+    uint gasWanted,
+    uint minFinishGas,
+    uint penaltyPerGas,
+    address maker
+  ) internal pure returns (Spec memory spec) {
+    return
+      Spec({
+        orderId: orderId,
+        wants: wants,
+        gives: gives,
+        gasWanted: gasWanted,
+        minFinishGas: minFinishGas,
+        penaltyPerGas: penaltyPerGas,
+        maker: maker
+      });
+  }
+
+  function testDex(Spec[] memory orderList) internal {
+    uint orderId = dex.best();
+    for (uint i = 0; i < orderList.length; i++) {
+      (
+        uint wants,
+        uint gives,
+        uint nextId,
+        uint gasWanted,
+        uint minFinishGas, // global minFinishGas at order creation time
+        uint penaltyPerGas, // global penaltyPerGas at order creation time
+        address maker
+      ) = dex.getOrderInfo(orderId);
+      Spec memory spec = orderList[i];
+      testEq(spec.orderId, orderId, "incorrect order Id");
+      testEq(spec.wants, wants, "incorrect wanted price");
+      testEq(spec.gives, gives, "incorrect give price");
+      testEq(spec.gasWanted, gasWanted, "incorrect gas wanted");
+      testEq(spec.minFinishGas, minFinishGas, "incorrect min finish gas");
+      testEq(spec.penaltyPerGas, penaltyPerGas, "incorrect penalty");
+      testEq(spec.maker, maker, "incorrect maker address");
+      orderId = nextId;
+    }
   }
 
   function _beforeAll() public {
@@ -150,22 +171,61 @@ contract Dex_Test is Test, Display {
     }
   }
 
-  function basicMarketOrder_test() public {
-    uint init_mkr_a_bal = aToken.balanceOf(address(maker));
-    uint init_mkr_b_bal = bToken.balanceOf(address(maker));
-    uint init_tkr_a_bal = aToken.balanceOf(address(taker));
-    uint init_tkr_b_bal = bToken.balanceOf(address(taker));
-
+  function newOrder(
+    TestMaker maker,
+    uint wants,
+    uint gives,
+    uint gasWanted,
+    uint pivotId
+  ) internal returns (Spec memory, uint) {
     uint orderId = maker.newOrder({
       wants: 1 ether,
       gives: 1 ether,
       gasWanted: 2300,
       pivotId: 0
     });
+    Spec memory spec = specOf({
+      orderId: orderId,
+      wants: 1 ether,
+      gives: 1 ether,
+      gasWanted: 2300,
+      minFinishGas: dex.minFinishGas(),
+      penaltyPerGas: dex.penaltyPerGas(),
+      maker: address(maker)
+    });
+    return (spec, orderId);
+  }
+
+  function basicMarketOrder_test() public {
+    uint init_mkr_a_bal = aToken.balanceOf(address(maker));
+    uint init_mkr_b_bal = bToken.balanceOf(address(maker));
+    uint init_tkr_a_bal = aToken.balanceOf(address(taker));
+    uint init_tkr_b_bal = bToken.balanceOf(address(taker));
+    (Spec memory spec0, uint orderId0) = newOrder({
+      maker: maker,
+      wants: 1 ether,
+      gives: 1 ether,
+      gasWanted: 2300,
+      pivotId: 0
+    });
+    (Spec memory spec1, uint orderId1) = newOrder({
+      maker: maker,
+      wants: 1 ether,
+      gives: 0.5 ether,
+      gasWanted: 2800,
+      pivotId: 0
+    });
+
+    Spec[] memory specOB = new Spec[](2);
+    specOB[0] = spec0; // best offer
+    specOB[1] = spec1;
+
+    // Testing correct insertion in OB
+    testDex(specOB);
 
     uint orderAmount = 0.5 ether;
 
-    taker.take({orderId: orderId, wants: orderAmount});
+    taker.take({orderId: orderId1, wants: orderAmount});
 
     uint expec_mkr_a_bal = init_mkr_a_bal - orderAmount;
     uint expec_mkr_b_bal = init_mkr_b_bal + orderAmount;
@@ -194,22 +254,22 @@ contract Dex_Test is Test, Display {
     );
   }
 
-  function moriartyMaketOrder_test() public {
-    // Maker adds dummy order
-    maker.newOrder({
-      wants: 1 ether,
-      gives: 1 ether,
-      gasWanted: 2300,
-      pivotId: 0
-    });
-
-    uint orderAmount = 0.5 ether;
-    try taker.mo({wants: orderAmount, gives: orderAmount})  {
-      testFail("taking moriarty offer should fail");
-    } catch Error(
-      string memory /*reason*/
-    ) {
-      testSuccess();
-    }
-  }
+  // function moriartyMaketOrder_test() public {
+  //   // Maker adds dummy order
+  //   maker.newOrder({
+  //     wants: 1 ether,
+  //     gives: 1 ether,
+  //     gasWanted: 2300,
+  //     pivotId: 0
+  //   });
+  //
+  //   uint orderAmount = 0.5 ether;
+  //   try taker.mo({wants: orderAmount, gives: orderAmount})  {
+  //     testFail("taking moriarty offer should fail");
+  //   } catch Error(
+  //     string memory /*reason*/
+  //   ) {
+  //     testSuccess();
+  //   }
+  // }
 }
