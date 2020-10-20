43899d4449818af56ad6442319bc814ed4a33985
Jean Krivine <Jean.Krivine@irif.fr>
2020-10-20 17:12:15 +0200

    MakerDeployer and removing specs

--- contracts/Dex_Test.sol
+++ contracts/Dex_Test.sol
@@ -79,89 +79,22 @@ contract Dex_Test is Test, Display {
   }
 
   function c_beforeAll() public {
-    (maker, evilMaker) = DexPre2.setup(dex);
+    makers = DexPre2.setup(dex);
+    makers.deploy(nMakers);
     taker = DexPre3.setup(dex);
-    address(maker).transfer(50 ether);
-    maker.provisionDex(10 ether);
-    address(evilMaker).transfer(50 ether);
-    evilMaker.provisionDex(10 ether);
+  }
 
-    aToken.mint(address(maker), 5 ether);
+  function d_beforeAll() public {
+    // low level tranfer because makers needs gas to transfer to each maker
+    (bool success, ) = address(makers).call{gas: gasleft(), value: 50 ether}(
+      ""
+    );
+    require(success, "maker transfer");
+
+    makers.provisionForAll(10 ether); // each maker provsions 10 ethers
+    makers.mintForAll(aToken, 5 ether);
     bToken.mint(address(taker), 5 ether);
-    maker.approve(aToken, 5 ether);
-    evilMaker.approve(aToken, 5 ether);
     taker.approve(bToken, 5 ether);
-  }
-
-  // orderList[i] starts with the orderId at position i, followed by order info
-  // minus next field.
-  struct Spec {
-    uint orderId;
-    uint wants;
-    uint gives;
-    uint gasWanted;
-    uint minFinishGas;
-    uint penaltyPerGas;
-    address maker;
-  }
-
-  function specOf(
-    uint orderId,
-    uint wants,
-    uint gives,
-    uint gasWanted,
-    uint minFinishGas,
-    uint penaltyPerGas,
-    address maker
-  ) internal pure returns (Spec memory spec) {
-    return
-      Spec({
-        orderId: orderId,
-        wants: wants,
-        gives: gives,
-        gasWanted: gasWanted,
-        minFinishGas: minFinishGas,
-        penaltyPerGas: penaltyPerGas,
-        maker: maker
-      });
-  }
-
-  function testDex(Spec[] memory orderList, string memory message) internal {
-    uint orderId = dex.best();
-    bool success = true;
-    for (uint i = 0; i < orderList.length; i++) {
-      (
-        uint wants,
-        uint gives,
-        uint nextId,
-        uint gasWanted,
-        uint minFinishGas, // global minFinishGas at order creation time
-        uint penaltyPerGas, // global penaltyPerGas at order creation time
-        address maker
-      ) = dex.getOrderInfo(orderId);
-      Spec memory spec = orderList[i];
-      success = success && testEq(spec.orderId, orderId, "incorrect order Id");
-      success = success && testEq(spec.wants, wants, "incorrect wanted price");
-      success = success && testEq(spec.gives, gives, "incorrect give price");
-      success =
-        success &&
-        testEq(spec.gasWanted, gasWanted, "incorrect gas wanted");
-      success =
-        success &&
-        testEq(spec.minFinishGas, minFinishGas, "incorrect min finish gas");
-      success =
-        success &&
-        testEq(spec.penaltyPerGas, penaltyPerGas, "incorrect penalty");
-      success = success && testEq(spec.maker, maker, "incorrect maker address");
-      if (success) {
-        testSuccess();
-      }
-      orderId = nextId;
-    }
-    if (success) {
-      console.logString(message);
-    }
-    logOrderBook(dex);
   }
 
   function zeroDust_test() public {
@@ -174,118 +107,60 @@ contract Dex_Test is Test, Display {
     }
   }
 
-  function newOrder(
-    TestMaker maker,
-    uint wants,
-    uint gives,
-    uint gasWanted,
-    uint pivotId
-  ) internal returns (Spec memory, uint) {
-    uint orderId = maker.newOrder({
-      wants: wants,
-      gives: gives,
-      gasWanted: gasWanted,
-      pivotId: pivotId
-    });
-    Spec memory spec = specOf({
-      orderId: orderId,
-      wants: wants,
-      gives: gives,
-      gasWanted: gasWanted,
-      minFinishGas: dex.minFinishGas(),
-      penaltyPerGas: dex.penaltyPerGas(),
-      maker: address(maker)
-    });
-    return (spec, orderId);
-  }
-
-  function takeFromSpec(Spec memory spec, uint taken)
-    internal
-    pure
-    returns (Spec memory)
-  {
-    return
-      Spec({
-        orderId: spec.orderId,
-        wants: spec.wants - ((taken * spec.wants) / spec.gives),
-        gives: spec.gives - taken,
-        gasWanted: spec.gasWanted,
-        minFinishGas: spec.minFinishGas,
-        penaltyPerGas: spec.penaltyPerGas,
-        maker: spec.maker
-      });
-  }
-
-  function take(
-    TestTaker taker,
-    uint orderId,
-    uint wants,
-    Spec memory spec
-  ) internal returns (Spec memory) {
-    uint taken = taker.take(orderId, wants);
-    Spec memory newspec = takeFromSpec(spec, taken);
-    return newspec;
-  }
-
   function basicMarketOrder_test() public {
-    uint init_mkr_a_bal = aToken.balanceOf(address(maker));
-    uint init_mkr_b_bal = bToken.balanceOf(address(maker));
-    uint init_tkr_a_bal = aToken.balanceOf(address(taker));
-    uint init_tkr_b_bal = bToken.balanceOf(address(taker));
-    (Spec memory spec0, uint orderId0) = newOrder({
-      maker: maker,
+    uint orderId;
+    TestMaker maker2 = makers.getMaker(2);
+
+    makers.getMaker(0).newOrder({
       wants: 1 ether,
       gives: 1 ether,
       gasWanted: 2300,
       pivotId: 0
     });
-    (Spec memory spec1, uint orderId1) = newOrder({
-      maker: maker,
+
+    orderId = maker2.newOrder({
       wants: 1 ether,
       gives: 0.5 ether,
-      gasWanted: 2800,
-      pivotId: 0
+      gasWanted: 8000,
+      pivotId: 1
     });
 
-    Spec[] memory specOB = new Spec[](2);
-    specOB[0] = spec0; // best offer
-    specOB[1] = spec1;
+    makers.getMaker(1).newOrder({
+      wants: 0.5 ether,
+      gives: 1 ether,
+      gasWanted: 7000,
+      pivotId: 2
+    });
 
-    // Testing correct insertion in OB
-    testDex(specOB, "OB has correctly inserted the 2 orders");
+    logOrderBook(dex);
+    uint orderAmount = 0.3 ether; //of a token
+    uint price = 1 ether / 0.5 ether;
 
-    uint orderAmount = 0.3 ether;
+    uint init_mkr_a_bal = aToken.balanceOf(address(maker2));
+    uint init_mkr_b_bal = bToken.balanceOf(address(maker2));
+    uint init_tkr_a_bal = aToken.balanceOf(address(taker));
+    uint init_tkr_b_bal = bToken.balanceOf(address(taker));
 
-    //    taker.take({orderId: orderId1, wants: orderAmount});
-
-    Spec memory newspec1 = take(taker, orderId1, orderAmount, spec1);
-    specOB[1] = newspec1;
-
-    // Testing correct update of OB
-    testDex(specOB, "OB has partially consumed order 1");
-
-    uint expec_mkr_a_bal = init_mkr_a_bal - orderAmount;
-    uint expec_mkr_b_bal = init_mkr_b_bal + orderAmount;
-    uint expec_tkr_a_bal = init_tkr_a_bal + orderAmount;
-    uint expec_tkr_b_bal = init_tkr_b_bal - orderAmount;
+    taker.take(orderId, orderAmount);
+    logOrderBook(dex);
 
     testEq(
-      expec_mkr_a_bal,
-      aToken.balanceOf(address(maker)),
+      init_mkr_a_bal - orderAmount,
+      aToken.balanceOf(address(makers.getMaker(2))),
       "incorrect maker A balance"
     );
     testEq(
-      expec_mkr_b_bal,
-      bToken.balanceOf(address(maker)),
+      init_mkr_b_bal + orderAmount * price,
+      bToken.balanceOf(address(makers.getMaker(2))),
       "incorrect maker B balance"
     );
     testEq(
-      expec_tkr_a_bal,
+      init_tkr_a_bal + orderAmount,
       aToken.balanceOf(address(taker)),
       "incorrect taker A balance"
     );
     testEq(
-      expec_tkr_b_bal,
+      init_tkr_b_bal - orderAmount * price,
       bToken.balanceOf(address(taker)),
       "incorrect taker B balance"
     );
