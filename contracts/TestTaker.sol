7070127c456688aea45450e4f36e14cc1f750d22
Jean Krivine <jean.krivine@irif.fr>
2020-11-05 17:40:34 +0100

    Test is a library

--- contracts/TestTaker.sol
+++ contracts/TestTaker.sol
@@ -5,29 +5,31 @@ import "./interfaces.sol";
 import "./Test.sol";
 import "./Dex.sol";
 
-contract TestTaker is ITaker,Test {
+contract TestTaker is ITaker {
   Dex dex;
 
   constructor(Dex _dex) {
     dex = _dex;
   }
 
-//  receive() external payable {}
+  receive() external payable {}
 
   function approve(IERC20 token, uint amount) external {
     token.approve(address(dex), amount);
   }
 
-  function take(uint orderId, uint takerWants) external override
-   returns (uint) {
-    (, uint makerGives,,,,,) =
-      dex.getOrderInfo(orderId);
-    uint taken = min(makerGives, takerWants);
+  function take(uint orderId, uint takerWants)
+    external
+    override
+    returns (uint)
+  {
+    (, uint makerGives, , , , , ) = dex.getOrderInfo(orderId);
+    uint taken = Test.min(makerGives, takerWants);
     dex.snipe(orderId, takerWants);
     return taken;
   }
 
-  function mo(uint wants, uint gives) external override {
+  function marketOrder(uint wants, uint gives) external override {
     dex.conditionalMarketOrder(wants, gives);
   }
 }
