407e7efae5e3352e8dd4398c358bf550355c041f
Jean Krivine <jean.krivine@irif.fr>
2020-11-05 17:37:28 +0100

    Adding uint2str conversion + append

--- contracts/Display.sol
+++ contracts/Display.sol
@@ -4,10 +4,41 @@ pragma solidity ^0.7.0;
 import "./Dex.sol";
 import "hardhat/console.sol";
 
-contract Display {
-  function logOrderBook(Dex dex) internal view {
+library Display {
+  function uint2str(uint _i)
+    internal
+    pure
+    returns (string memory _uintAsString)
+  {
+    if (_i == 0) {
+      return "0";
+    }
+    uint j = _i;
+    uint len;
+    while (j != 0) {
+      len++;
+      j /= 10;
+    }
+    bytes memory bstr = new bytes(len);
+    uint k = len - 1;
+    while (_i != 0) {
+      bstr[k--] = byte(uint8(48 + (_i % 10)));
+      _i /= 10;
+    }
+    return string(bstr);
+  }
+
+  function append(string memory a, string memory b)
+    external
+    pure
+    returns (string memory)
+  {
+    return string(abi.encodePacked(a, b));
+  }
+
+  function logOrderBook(Dex dex) external view {
     uint orderId = dex.best();
-    console.log("----------");
+    console.log("-----Best order: %d-----", dex.getBest());
     while (orderId != 0) {
       (
         uint wants,
@@ -33,6 +64,6 @@ contract Display {
       console.logAddress(makerAddr);
       orderId = nextId;
     }
-    console.log("----------");
+    console.log("-----------------------");
   }
 }
