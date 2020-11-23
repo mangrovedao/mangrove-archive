1a08de4740830071379b06cfbf8ccb9b0650c8e0
Adrien Husson <adhusson@gmail.com>
2020-11-23 19:16:33 +0100

    Add Money Market agent

--- contracts/Tests/Agents/TestToken.sol
+++ contracts/Tests/Agents/TestToken.sol
@@ -5,18 +5,27 @@ pragma solidity ^0.7.0;
 import "../../ERC20.sol";
 
 contract TestToken is ERC20 {
-  address admin;
+  mapping(address => bool) admins;
 
   constructor(
     address _admin,
     string memory name,
     string memory symbol
   ) ERC20(name, symbol) {
-    admin = _admin;
+    admins[_admin] = true;
+  }
+
+  function requireAdmin() internal view {
+    require(admins[msg.sender], "TestToken/adminOnly");
+  }
+
+  function addAdmin(address admin) external {
+    requireAdmin();
+    admins[admin] = true;
   }
 
   function mint(address to, uint amount) external {
-    require(msg.sender == admin, "non-admin minting");
+    requireAdmin();
     _mint(to, amount);
   }
 }
