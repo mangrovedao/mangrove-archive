6cb9e3a72da853b24dc37c8a75ff409e567b9a59
Adrien Husson <adhusson@gmail.com>
2020-12-03 22:43:21 +0100

    Event expectations are about preceding events

--- contracts/Tests/Toolbox/TestEvents.sol
+++ contracts/Tests/Toolbox/TestEvents.sol
@@ -14,7 +14,21 @@ library TestEvents {
 
   event ExpectFrom(address from);
 
-  /* Usage: from a test contract t, call `expectFrom(a)`. Any subsequent non-special event emitted by t will mean "I expect a to emit the exact same event". The order of expectations must be respected. */
+  /* Usage: from a test contract t, call `expectFrom(a)` with a != t (otherwise undefined behaviour). Any subsequent non-special event emitted by t will mean "I expect a to have already emitted the exact same event". The order of expectations must be respected. 
+     Formally:
+     * let (e1, e2, ...) be the sequence ordering the events yielded by the transaction
+     * let tests be all events emitted by the testing contract
+     * let instructions be all tests interpreted by javascript
+     * let froms be instructions with signature ExpectFrom(address a)
+     * let regulars be all non-test events, then
+     * let expects be all non-instruction tests that occur after at least one from, and for all expects e,
+     * from(e) is the latest such from, and
+     * match(e) is the earliest regular e' such that e' happens before e and e and e' are equal.
+     The following must be true: 
+     1) if match(e) is emitted by a, from(e) = ExpectFrom(a)
+     2) match is a total function
+     3) match is an order-preserving function
+  */
   function expectFrom(address from) internal {
     emit ExpectFrom(from);
   }
