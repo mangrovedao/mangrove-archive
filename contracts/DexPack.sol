94a9e8784c782406f52ef6f81f158e6769ee3b23
Adrien Husson <adhusson@gmail.com>
2021-01-21 17:33:37 +0100

    Remove false guard and add pack/unpack functions. Prettier ignore new DexPack.

---
+++ contracts/DexPack.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+library DexPack {
+
+  // $for ns in [['writeOffer',writeOffer],['offer',offer],['offerDetail',offerDetail],['global',global],['local',local]]
+  // prettier-ignore
+  function $$(ns[0])_pack($$(join(map(ns[1],(fb) => `$${fb[2]} __$${fb[0]}`),', '))) internal pure returns (bytes32) {
+    return $$(set_unsafe('bytes32(0)',ns[1],map(ns[1], (fb) => [fb[0],`__$${fb[0]}`])));
+  }
+
+  // prettier-ignore
+  function $$(ns[0])_unpack(bytes32 __packed) internal pure returns ($$(join(map(ns[1],(fb) => `$${fb[2]} __$${fb[0]}`),', '))) {
+    // $for fb in ns[1]
+    __$$(fb[0]) = $$(get('__packed',ns[1],fb[0]));
+    // $done
+  }
+  // $done
+}
