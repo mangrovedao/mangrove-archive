b86666d6f1b76eb4006986a51940f02a05270b12
Jean Krivine <Jean.Krivine@irif.fr>
2021-01-29 11:43:31 +0100

    Completing TakerOperations tests

--- contracts/Tests/TakerOperations.sol
+++ contracts/Tests/TakerOperations.sol
@@ -381,89 +381,22 @@ contract TakerOperations_Test {
     );
   }
 
-  //
-  // function unsafe_gas_left_fails_order_test() public {
-  //   dex.setGasbase(1);
-  //   tkr.approve(quote, 1 ether);
-  //   uint ofr = mkr.newOffer(1 ether, 1 ether, 120_000, 0);
-  //   try tkr.take{gas: 120_000}(ofr, 1 ether) {
-  //     TestEvents.fail("unsafe gas amount, order should fail");
-  //   } catch Error(string memory r) {
-  //     TestEvents.eq(r, "dex/unsafeGasAmount", "wrong revert reason");
-  //   }
-  // }
-  //
-  // function taker_hasnt_approved_A_fails_order_test() public {
-  //   dex.setFee(address(base), address(quote), 300);
-  //   tkr.approve(quote, 1 ether);
-  //   uint ofr = mkr.newOffer(1 ether, 1 ether, 100_000, 0);
-  //   try tkr.take(ofr, 1 ether) {
-  //     TestEvents.fail("Taker hasn't approved for A, order should fail");
-  //   } catch Error(string memory r) {
-  //     TestEvents.eq(r, "dex/takerFailToPayDex", "wrong revert reason");
-  //   }
-  // }
-  //
-  // /* This test uses an ERC20 with callback and an evil taker to take out `base` received as soon as they come in. It does not make sense with a non-inverted Dex and a flashloan system based on checking balanceOf, because ERC20+callback+evilTaker means there is no way for the maker to defend against a bad taker. A version of this test could be restored in the inverted dex case, because a variant of evil maker could remove base tokens *during* its `execute` call. But with a normal Dex, we're essentially testing the ERC20 which makes no sense. */
-  // //function taker_has_no_A_fails_order_test() public {
-  // //tkr.setEnabled(true);
-  // //dex.setFee(address(base), address(quote), 300);
-  // //tkr.approve(quote, 1 ether);
-  // //tkr.approve(base, 1 ether);
-  // //uint ofr = mkr.newOffer(1 ether, 1 ether, 100_000, 0);
-  // //try tkr.take(ofr, 1 ether) {
-  // //TestEvents.fail("Taker doesn't have enough A, order should fail");
-  // //} catch Error(string memory r) {
-  // //TestEvents.eq(r, "dex/takerFailToPayDex", "wrong revert reason");
-  // //}
-  // //}
-  //
-  // function marketOrder_on_empty_book_fails_test() public {
-  //   try tkr.marketOrder(1 ether, 1 ether) {
-  //     TestEvents.fail("market order on empty book should fail");
-  //   } catch Error(string memory r) {
-  //     TestEvents.eq(r, "dex/marketOrder/noSuchOffer", "wrong revert reason");
-  //   }
-  // }
-  //
-  // function marketOrder_with_bad_offer_id_fails_test() public {
-  //   try tkr.marketOrderWithFail(1 ether, 1 ether, 0, 43) {
-  //     TestEvents.fail("market order wit bad offer id should fail");
-  //   } catch Error(string memory r) {
-  //     TestEvents.eq(r, "dex/marketOrder/noSuchOffer", "wrong revert reason");
-  //   }
-  // }
-  //
-  // function taking_same_offer_twice_fails_test() public {
-  //   tkr.approve(quote, 1 ether);
-  //   uint ofr = mkr.newOffer(1 ether, 1 ether, 100_000, 0);
-  //   tkr.take(ofr, 1 ether);
-  //   try tkr.marketOrderWithFail(0, 0, 0, ofr) {
-  //     TestEvents.fail("Offer should have been deleted");
-  //   } catch Error(string memory r) {
-  //     TestEvents.eq(r, "dex/marketOrder/noSuchOffer", "wrong revert reason");
-  //   }
-  // }
-  //
-  // function small_partial_fill_can_be_retaken_test() public {
-  //   tkr.approve(quote, 1 ether);
-  //   dex.setDensity(address(base), address(quote), 1);
-  //   dex.setGasbase(1);
-  //   uint ofr = mkr.newOffer(100_002, 100_002, 100_000, 0);
-  //   tkr.take(ofr, 1);
-  //   tkr.marketOrderWithFail(100_001, 100_001, 0, ofr);
-  // }
-  //
-  // function big_partial_fill_cant_be_retaken_test() public {
-  //   tkr.approve(quote, 1 ether);
-  //   dex.setDensity(address(base), address(quote), 1);
-  //   dex.setGasbase(1);
-  //   uint ofr = mkr.newOffer(100_001, 100_001, 100_000, 0);
-  //   tkr.take(ofr, 2);
-  //   try tkr.marketOrderWithFail(100_001, 100_001, 0, ofr) {
-  //     TestEvents.fail("Offer should have been deleted");
-  //   } catch Error(string memory r) {
-  //     TestEvents.eq(r, "dex/marketOrder/noSuchOffer", "wrong revert reason");
-  //   }
-  // }
+  function unsafe_gas_left_fails_order_test() public {
+    dex.setGasbase(base, quote, 1);
+    quoteT.approve(address(dex), 1 ether);
+    uint ofr = mkr.newOffer(1 ether, 1 ether, 120_000, 0);
+    try dex.snipe{gas: 120_000}(base, quote, ofr, 1 ether, 1 ether, 120_000) {
+      TestEvents.fail("unsafe gas amount, order should fail");
+    } catch Error(string memory r) {
+      TestEvents.eq(r, "dex/notEnoughGasForMakerTrade", "wrong revert reason");
+    }
+  }
+
+  function marketOrder_on_empty_book_returns_test() public {
+    try dex.simpleMarketOrder(base, quote, 1 ether, 1 ether) {
+      TestEvents.succeed();
+    } catch Error(string memory r) {
+      TestEvents.fail("market order on empty book should not fail");
+    }
+  }
 }
