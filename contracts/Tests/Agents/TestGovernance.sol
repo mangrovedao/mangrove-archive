b2e3d489e00b0cac0ceab4f08d02ae966404c5d4
Adrien Husson <adhusson@gmail.com>
2021-02-03 12:53:40 +0100

    Add TestGovernance (forgot to add it in earlier commit)

---
+++ contracts/Tests/Agents/TestGovernance.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.0;
+pragma abicoder v2;
+
+import "../../DexCommon.sol";
+
+library L {
+  event GovSuccess(DexCommon.SingleOrder order, address taker);
+  event GovFail(DexCommon.SingleOrder order);
+}
+
+contract DexGovernance is IDexGovernance {
+  uint gasprice;
+  mapping(address => mapping(address => uint)) private densities;
+
+  function setGasprice(uint _gasprice) external {
+    gasprice = _gasprice;
+  }
+
+  function setDensity(
+    address base,
+    address quote,
+    uint _density
+  ) external {
+    densities[base][quote] = _density;
+  }
+
+  function read(address base, address quote)
+    external
+    override
+    returns (uint, uint)
+  {
+    return (gasprice, densities[base][quote]);
+  }
+
+  function notifySuccess(DexCommon.SingleOrder calldata sor, address taker)
+    external
+    override
+  {
+    emit L.GovSuccess(sor, taker);
+  }
+
+  function notifyFail(DexCommon.SingleOrder calldata sor) external override {
+    emit L.GovFail(sor);
+  }
+}
