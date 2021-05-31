04fd6fff6e8e3627e9bbfd652976f673a4bc079b
Jean Krivine <jean.krivine@irif.fr>
2021-05-31 17:16:23 +0200

    giving full uin256 storage capacity to prices

--- contracts/librairies/PriceOracle.sol
+++ contracts/librairies/PriceOracle.sol
@@ -3,28 +3,22 @@ pragma abicoder v2;
 import "./AccessControlled.sol";
 
 contract PriceOracle is AccessControlled {
-  address immutable base_token;
   event QuoteReceived(uint);
   event Subscribed(address);
 
-  mapping(address => uint160) private quotes;
+  mapping(address => uint) private quotes;
   mapping(address => bool) public subscribers;
-
-  constructor(address _base_token) AccessControlled() {
-    base_token = _base_token;
-  }
 
   function register(address subscriber) external onlyCaller(admin) {
     subscribers[subscriber] = true;
   }
 
-  // price in quote for 1 unit of base.
+  // price in quote for 1 exaunit of base.
   function set_quote_for(address erc_quote, uint amount)
     external
     onlyCaller(admin)
   {
-    require(uint160(amount) == amount);
-    quotes[erc_quote] = uint160(amount);
+    quotes[erc_quote] = amount;
     emit QuoteReceived(amount);
   }
 
