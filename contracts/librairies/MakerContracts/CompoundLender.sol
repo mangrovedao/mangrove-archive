1cd8024aa3d0c1b1898dce0cbeaafabca5a054f8
Jean Krivine <jean.krivine@irif.fr>
2021-06-11 17:57:58 +0200

    compoundLender __put__ and __get__

--- contracts/librairies/MakerContracts/CompoundLender.sol
+++ contracts/librairies/MakerContracts/CompoundLender.sol
@@ -4,21 +4,20 @@ import "./MangroveOffer.sol";
 import "./CompoundInterface.sol";
 
 contract CompoundLender is MangroveOffer, Exponential {
-  event UnexpErrorOnRedeem(address cToken, uint amount);
   event ErrorOnRedeem(address cToken, uint amount, uint errorCode);
-  event UnexpErrorOnDeposit(address cToken, uint amount);
-  event ErrorOnDeposit(address cToken, uint amount);
+  event ErrorOnMint(address cToken, uint amount, uint errorCode);
+  event ComptrollerError(address comp, uint errorCode);
 
   // mapping : ERC20 -> cERC20
   mapping(address => address) public overlyings;
-  mapping(address => bool) public compoundPut;
-  mapping(address => bool) public compoundGet;
+  mapping(address => bool) public compoundPutFlag;
+  mapping(address => bool) public compoundGetFlag;
 
   // address of the comptroller
-  IComptroller immutable public comptroller;
+  IComptroller public immutable comptroller;
 
   // address of the price oracle used by the comptroller
-  ICompoundPriceOracle immutable public oracle;
+  ICompoundPriceOracle public immutable oracle;
 
   constructor(address _comptroller, address payable _MGV) MangroveOffer(_MGV) {
     comptroller = IComptroller(_comptroller); // comptroller address
@@ -28,112 +27,129 @@ contract CompoundLender is MangroveOffer, Exponential {
   ///@notice To declare put/get methods should use Compound to manage token assets
   ///@param token address of the underlying token
   ///@param cToken address of the overlying token. Put 0x0 here to stop getting/putting token on Compound
-  function _setCompoundSource(address token, address cToken)
+  function setCompoundSource(address token, address cToken)
     external
     onlyCaller(admin)
   {
     overlyings[token] = cToken;
   }
 
-  function _setCompoundPut(address erc20, bool flag) external onlyCaller(admin) {
-    compoundPut[erc20] = flag;
+  function setCompoundPutFlag(address erc20, bool flag)
+    external
+    onlyCaller(admin)
+  {
+    compoundPutFlag[erc20] = flag;
   }
 
-  function _setCompoundGet(address erc20, bool flag) external onlyCaller(admin) {
-    compoundGet[erc20] = flag;
+  function setCompoundGetFlag(address erc20, bool flag)
+    external
+    onlyCaller(admin)
+  {
+    compoundGetFlag[erc20] = flag;
   }
 
-
-  function maxGettableUnderlying(IcERC20 cToken) internal view returns (uint, uint) {
-    
-    (uint , uint liquidity, uint) = comptroller.getAccountLiquidity(msg.sender);
-    uint maxRedeemableUnderlying = divScalarByExpTruncate(
-      liquidity,
-      mulExp(
-        Exp({exp:comptroller.markets(cToken).collateralFactorMantissa}),
-        Exp({exp:oracle.getUnderlyingPrice(cToken)});
-      )
-    );
-    return (liquidity,maxRedeemableUnderlying);
+  /// @notice Returns maximal borrow capacity of the account and maximal redeem capacity
+  /// @notice accrues interests of compound
+  function maxGettableUnderlying(IcERC20 cToken)
+    internal
+    view
+    returns (uint, uint)
+  {
+    // gets account liquidity in USD units
+    (uint errCode, uint liquidity, ) =
+      comptroller.getAccountLiquidity(msg.sender);
+    if (errCode != 0) {
+      return (0, 0);
+    }
+    // maxRedeem = liquidity / (CF_underlying * price_underlying)
+    (, uint collateralFactorMantissa, ) = comptroller.markets(address(cToken));
+    (, uint maxRedeemableUnderlying) =
+      divScalarByExpTruncate(
+        liquidity,
+        mul_(
+          Exp({mantissa: collateralFactorMantissa}),
+          Exp({mantissa: oracle.getUnderlyingPrice(cToken)})
+        )
+      );
+    return (liquidity, maxRedeemableUnderlying);
   }
 
   ///@notice method to get `base` during makerTrade
   ///@param base address of the ERC20 managing `base` token
   ///@param amount of token that the trade is still requiring
-  function get(address base, uint amount) internal override returns (uint) {
-    if (!_compoundGet[base]) { // if flag says not to fetch liquidity on compound
+  function __get__(address base, uint amount)
+    internal
+    virtual
+    override
+    returns (uint)
+  {
+    if (!compoundGetFlag[base]) {
+      // if flag says not to fetch liquidity on compound
       return amount;
     }
-    address base_cErc20 = _overlyings[base]; // this is 0x0 if base is not compound sourced.
-    if (base_cErc20 == address(0)) { 
-      return (0,0);
+    address base_cErc20 = overlyings[base]; // this is 0x0 if base is not compound sourced.
+    if (base_cErc20 == address(0)) {
+      return amount;
     }
-    (uint, uint redeemable) = maxGettableUnderlying(base);  
+    (, uint redeemable) = maxGettableUnderlying(IcERC20(base_cErc20));
     uint redeemAmount = redeemable > amount ? amount : redeemable;
-    getInternal()
-  
+    return compoundRedeem(base_cErc20, redeemAmount);
   }
 
-  function getInternal(address cBase, uint amountToRedeem) internal returns (uint){
-    try IcERC20(cBase).redeemUnderlying(amountToRedeem) returns (
-        uint errorCode
-      ) {
-        if (errorCode == 0) {
-          //compound redeem was a success
-          return 0;
-        } else {
-          //ompound redeem failed
-          emit ErrorOnRedeem(cBase, amountToRedeem, errorCode);
-          return amountToRedeem;
-        }
-      } catch {
-        emit UnexpErrorOnRedeem(base_cErc20, amountToRedeem);
-        return amountToRedeem;
-      }
+  function compoundRedeem(address cBase, uint amountToRedeem)
+    internal
+    returns (uint)
+  {
+    uint errorCode = IcERC20(cBase).redeemUnderlying(amountToRedeem);
+    if (errorCode == 0) {
+      //compound redeem was a success
+      return 0;
+    } else {
+      //ompound redeem failed
+      emit ErrorOnRedeem(cBase, amountToRedeem, errorCode);
+      return amountToRedeem;
+    }
+  }
+
+  function __put__(address quote, uint amount)
+    internal
+    virtual
+    override
+    returns (uint)
+  {
+    //optim
+    if (amount == 0) {
+      return 0;
+    }
+    if (!compoundPutFlag[quote]) {
+      return amount;
+    }
+    address cToken = overlyings[quote];
+    if (cToken != address(0)) {
+      return compoundMint(quote, cToken, amount);
+    } else {
+      return amount;
+    }
   }
 
   // adapted from https://medium.com/compound-finance/supplying-assets-to-the-compound-protocol-ec2cf5df5aa#afff
   // utility to supply erc20 to compound
-  // NB `_cErc20` contract MUST be approved to perform `transferFrom _erc20` by `this` contract.
-  // `_cERC20` need not be `BASE_cERC` if LP wants to put quote payment into compound as well.
-  function supplyErc20ToCompound(address cErc20, uint numTokensToSupply)
-    external
-    returns (bool success)
-  {
-    address underlying = IcERC20(cErc20).underlying();
-    require(underlying != address(0), "Invalid cErc20 address");
-
+  // NB `cToken` contract MUST be approved to perform `transferFrom token` by `this` contract.
+  /// @notice user need to approve cToken in order to mint
+  function compoundMint(
+    address token,
+    address cToken,
+    uint amount
+  ) internal returns (uint missing) {
     // Approve transfer on the ERC20 contract (not needed if cERC20 is already approved for `this`)
-    IERC20(underlying).approve(cErc20, numTokensToSupply);
-
+    // IERC20(token).approve(cToken, amount);
+    uint errCode = IcERC20(cToken).mint(amount);
     // Mint cTokens
-    uint mintResult = IcERC20(cErc20).mint(numTokensToSupply);
-    success = (mintResult == 0);
-  }
-
-  function put(address quote, uint amount) internal override returns (bool) {
-    //optim
-    if (amount == 0) {
-      return true;
+    if (errCode == 0) {
+      return 0;
+    } else {
+      emit ErrorOnMint(cToken, amount, errCode);
+      return amount;
     }
-
-    address cToken = isCompoundSourced[quote];
-    (, uint cTokenBal, uint debt, ) =
-      IcERC20(cToken).getAccountSnapshot(msg.sender);
-
-    if (cToken != address(0)) {
-      try this.supplyErc20ToCompound(cToken, amount) returns (bool success) {
-        if (success) {
-          return true;
-        } else {
-          emit ErrorOnDeposit(cToken, amount);
-          return false;
-        }
-      } catch {
-        emit UnexpErrorOnDeposit(cToken, amount);
-        return false;
-      }
-    } //quote is not compound sourced
-    return super.put(quote, amount); //trying other deposit methods
   }
 }
