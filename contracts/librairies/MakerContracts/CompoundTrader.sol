4851ad3ccdd75a44e3fa3e062be7787867d8f422
Jean Krivine <jean.krivine@irif.fr>
2021-06-11 17:58:22 +0200

    compoundTrader __put__ and __get__

--- contracts/librairies/MakerContracts/CompoundTrader.sol
+++ contracts/librairies/MakerContracts/CompoundTrader.sol
@@ -3,70 +3,84 @@ pragma abicoder v2;
 import "./CompoundLender.sol";
 
 contract CompoundTrader is CompoundLender {
-  
-
   constructor(address _comptroller, address payable _MGV)
-  CompoundLender(_comptroller,_MGV){}
+    CompoundLender(_comptroller, _MGV)
+  {}
+
+  event ErrorOnBorrow(address cToken, uint amount, uint errorCode);
+  event ErrorOnRepay(address cToken, uint amount, uint errorCode);
 
   ///@notice method to get `base` during makerTrade
   ///@param base address of the ERC20 managing `base` token
   ///@param amount of token that the trade is still requiring
-  function get(address base, uint amount) internal override returns (GetResult, uint) {
-
-    if (!_compoundGet[base]) {return (GetResult.OK, amount);}
-    address base_cErc20 = _overlyings[base]; // this is 0x0 if base is not compound sourced for borrow.
-
-    if (base_cErc20 == address(0)) {return (GetResult.Error, amount);}
-
-    // 1. computing available liquidity
-    (uint err, uint liquidity, uint) = comptroller.getAccountLiquidity(msg.sender);
-
-    if (err != 0) {return (GetResult.Error, amount);} 
-    if (liquidity == 0) {return (GetResult.OK, amount);} 
-
-    // 2. Computing get capacity
-    uint gettableUnderlying = divScalarByExpTruncate(
-      liquidity,
-      Exp({mantissa:oracle.getUnderlyingPrice(IcERC20(base_cErc20))})
-    );
-    // 3. trying to mint liquidity from Compound
-    gettableUnderlying = gettableUnderlying > amount ? amount : gettableUnderlying;
-    uint got = super.get(base, gettableUnderlying);
-    
-    // 4. trying to borrow missing liquidity
-    try IcERC20(base_cErc20).borrow(gettableUnderlying - got) returns (uint errorCode) {
-      if (errorCode != 0) {
-        return (GetResult.Error, amount - got); // unable to borrow requested amount
-      }
-      return (GetResult.OK, amount - gettableUnderlying);
-    } catch {
-      (GetResult.FatalError, amount - got); // should revert trade
+  function __get__(address base, uint amount)
+    internal
+    virtual
+    override
+    returns (uint)
+  {
+    if (!compoundGetFlag[base]) {
+      return amount;
     }
+    address base_cErc20 = overlyings[base]; // this is 0x0 if base is not compound sourced for borrow.
+
+    if (base_cErc20 == address(0)) {
+      return amount;
+    }
+
+    // 1. Computing total borrow and redeem capacities
+    (uint gettable, uint redeemable) =
+      maxGettableUnderlying(IcERC20(base_cErc20));
+
+    // 2. trying to redeem liquidity from Compound
+    uint toRedeem = redeemable > amount ? amount : redeemable;
+    uint notRedeemed = compoundRedeem(base, toRedeem);
+    if (notRedeemed > 0 && toRedeem > 0) {
+      // this should not fail unless compound is out of cash
+      // log already emitted by `compoundRedeem`
+      return amount;
+    }
+    // borrowable = gettable - toRedeem
+    uint toBorrow = sub_(gettable, toRedeem);
+
+    // 3. trying to borrow missing liquidity
+    uint errorCode = IcERC20(base_cErc20).borrow(toBorrow);
+    if (errorCode != 0) {
+      emit ErrorOnBorrow(base_cErc20, toBorrow, errorCode);
+      return sub_(amount, toRedeem); // unable to borrow requested amount
+    }
+    return sub_(amount, add_(toRedeem, toBorrow));
   }
 
-  function put(address quote, uint amount) internal override returns (bool) {
+  /// @notice user need to have approved `quote` overlying in order to repay borrow
+  function __put__(address quote, uint amount)
+    internal
+    virtual
+    override
+    returns (uint)
+  {
     //optim
     if (amount == 0) {
-      return true;
+      return 0;
+    }
+    if (!compoundPutFlag[quote]) {
+      return amount;
+    }
+    IcERC20 cQuote = IcERC20(overlyings[quote]);
+    if (address(cQuote) == address(0)) {
+      return amount;
     }
 
-    address cToken = isCompoundSourced[quote];
-    (, uint cTokenBal, uint debt, ) =
-      IcERC20(cToken).getAccountSnapshot(msg.sender);
-
-    if (cToken != address(0)) {
-      try this.supplyErc20ToCompound(cToken, amount) returns (bool success) {
-        if (success) {
-          return true;
-        } else {
-          emit ErrorOnDeposit(cToken, amount);
-          return false;
-        }
-      } catch {
-        emit UnexpErrorOnDeposit(cToken, amount);
-        return false;
-      }
-    } //quote is not compound sourced
-    return super.put(quote, amount); //trying other deposit methods
+    uint repayable = cQuote.borrowBalanceCurrent(msg.sender);
+    uint toRepay = repayable > amount ? amount : repayable;
+    uint toMint;
+    uint errCode = cQuote.repayBorrow(toRepay);
+    if (errCode != 0) {
+      emit ErrorOnRepay(address(cQuote), toRepay, errCode);
+      toMint = amount;
+    } else {
+      toMint = amount - toRepay;
+    }
+    return compoundMint(quote, address(cQuote), toMint);
   }
 }
