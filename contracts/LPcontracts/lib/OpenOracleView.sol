38fa88e30fbbefcd57fefe2158ac7b84f9ab72d1
Jean Krivine <jean.krivine@irif.fr>
2021-06-22 09:00:21 +0200

    silencing a warning

--- contracts/LPcontracts/lib/OpenOracleView.sol
+++ contracts/LPcontracts/lib/OpenOracleView.sol
@@ -9,27 +9,27 @@ import "./OpenOracleData.sol";
  * @author Compound Labs, Inc.
  */
 contract OpenOracleView {
-    /**
-     * @notice The Oracle Data Contract backing this View
-     */
-    OpenOracleData public priceData;
+  /**
+   * @notice The Oracle Data Contract backing this View
+   */
+  OpenOracleData public priceData;
 
-    /**
-     * @notice The static list of sources used by this View
-     * @dev Note that while it is possible to create a view with dynamic sources,
-     *  that would not conform to the Open Oracle Standard specification.
-     */
-    address[] public sources;
+  /**
+   * @notice The static list of sources used by this View
+   * @dev Note that while it is possible to create a view with dynamic sources,
+   *  that would not conform to the Open Oracle Standard specification.
+   */
+  address[] public sources;
 
-    /**
-     * @notice Construct a view given the oracle backing address and the list of sources
-     * @dev According to the protocol, Views must be immutable to be considered conforming.
-     * @param data_ The address of the oracle data contract which is backing the view
-     * @param sources_ The list of source addresses to include in the aggregate value
-     */
-    constructor(OpenOracleData data_, address[] memory sources_) public {
-        require(sources_.length > 0, "Must initialize with sources");
-        priceData = data_;
-        sources = sources_;
-    }
+  /**
+   * @notice Construct a view given the oracle backing address and the list of sources
+   * @dev According to the protocol, Views must be immutable to be considered conforming.
+   * @param data_ The address of the oracle data contract which is backing the view
+   * @param sources_ The list of source addresses to include in the aggregate value
+   */
+  constructor(OpenOracleData data_, address[] memory sources_) {
+    require(sources_.length > 0, "Must initialize with sources");
+    priceData = data_;
+    sources = sources_;
+  }
 }
