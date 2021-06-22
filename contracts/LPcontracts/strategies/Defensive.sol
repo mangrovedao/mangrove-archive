4223395ff301300f9ec88dbe83073f86a8c655c4
Jean Krivine <jean.krivine@irif.fr>
2021-06-22 09:01:40 +0200

    defensive strategy

--- contracts/LPcontracts/strategies/Defensive.sol
+++ contracts/LPcontracts/strategies/Defensive.sol
@@ -8,8 +8,8 @@ import "../lib/OpenOraclePriceData.sol";
 contract Defensive is MangroveOffer, Exponential, OpenOraclePriceData {
   OpenOraclePriceData immutable priceFeed;
   address immutable trustedSource;
-  address constant coinbaseReporter =
-    0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC;
+  uint16 slippage;
+  uint constant BP = 1000;
 
   constructor(
     address _priceFeed,
@@ -20,29 +20,43 @@ contract Defensive is MangroveOffer, Exponential, OpenOraclePriceData {
     trustedSource = _trustedSource;
   }
 
+  function setSlippage(uint _slippage) external onlyAdmin {
+    require(uint16(_slippage) == _slippage, "Slippage overflow");
+    slippage = uint16(_slippage);
+  }
+
   function __lastLook__(MgvLib.SingleOrder calldata order)
     internal
+    view
     override
-    returns (uint)
   {
     IERC20 base = IERC20(order.base);
     IERC20 quote = IERC20(order.quote);
-    uint usd_wanted =
+    uint oracle_gives =
       mul_( //amount of base tokens required by taker (in ~USD, 6 decimals)
         order.wants,
         uint(priceFeed.getPrice(trustedSource, base.symbol())) //could be checking age of the time stamp of data
       );
-    uint usd_given =
+    uint oracle_wants =
       mul_( //amount of quote tokens given by taker (in ~USD, 6 decimals)
         order.gives,
         uint(priceFeed.getPrice(trustedSource, quote.symbol()))
       );
-
-    // oracle_price = usd_given/usd_wanted, order_price = order.gives/order.wants
-    // if oracle_price < offer_price return true
-    // else if order_price - oracle_price <= oracle_price * slippage (in %) return true (a)
-    // else return false
-    // (a) is iff usd_given * order.wants - usd_wanted * order.gives > order.wants * order.gives * slippage * 10^slippage_decimals
-    return 0;
+    uint offer_wants = order.gives; //uint96
+    uint offer_gives = order.wants; //uint96
+    // if p'=oracle_wants/oracle_gives > p=offer_wants/offer_gives
+    // we require p'-p > p*slippage/BP
+    // which is (oracle_gives * offer_wants * slippage)/BP - offer_gives * oracle_wants + oracle_gives*offer_wants > 0
+    uint oracleWantsTimesOfferGives = oracle_wants * offer_gives; // both are uint96 cannot overflow
+    uint offerWantsTimesOracleGives = offer_wants * oracle_gives; // both are uint96 cannot overflow
+    if (
+      (offerWantsTimesOracleGives * slippage) /
+        BP +
+        offerWantsTimesOracleGives <
+      oracleWantsTimesOfferGives
+    ) {
+      //revert if price is beyond slippage
+      failTrade(Fail.LastLook, uint120(oracle_wants), uint120(oracle_gives)); //passing fail data to __finalize__
+    }
   }
 }
