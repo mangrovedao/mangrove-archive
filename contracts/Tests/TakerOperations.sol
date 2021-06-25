dc31384c74fb61528c8c59e55739e65b716b0c61
Jean Krivine <Jean.Krivine@irif.fr>
2021-06-25 16:56:59 +0200

    adding gives=0 and wants=0 tests for taker

--- contracts/Tests/TakerOperations.sol
+++ contracts/Tests/TakerOperations.sol
@@ -652,4 +652,24 @@ contract TakerOperations_Test {
       "mgv should not be locked after marketOrder on empty OB"
     );
   }
+
+  function takerWants_is_zero_succeeds_test() public {
+    try mgv.marketOrder(base, quote, 0, 1 ether) returns (uint got, uint gave){
+      TestEvents.eq(got,0,"Taker got too much");
+      TestEvents.eq(gave,0 ether,"Taker gave too much");
+    } catch Error(string memory r) {
+      TestEvents.fail("Unexpected revert");
+    }
+  }
+
+  function takerGives_is_zero_succeeds_test() public {
+    try mgv.marketOrder(base, quote, 1 ether, 0) returns (uint got, uint gave){
+      TestEvents.eq(got,0,"Taker got too much");
+      TestEvents.eq(gave,0 ether,"Taker gave too much");
+    } catch Error(string memory r) {
+      TestEvents.fail("Unexpected revert");
+    }
+  }
+
+
 }
