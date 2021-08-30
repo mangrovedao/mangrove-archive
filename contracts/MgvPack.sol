e03fd377fad3bc790d6d71595709c93b7e3e3ec2
Adrien Husson <adhusson@gmail.com>
2021-08-30 13:07:04 +0200

    Clearer preprocessing code
    * replace `struct` -> `struct_def`
    * access struct field infos with names (.name) instead of indices ([i])

--- contracts/MgvPack.sol
+++ contracts/MgvPack.sol
@@ -5,34 +5,34 @@ library MgvPack {
 
   // fields are of the form [name,bits,type]
 
-  // $for ns in structs
+  // $for ns in struct_defs
 
   // $def sname ns[0]
   // $def scontents ns[1]
   /* $def arguments
-    join(map(scontents,(field) => `$${field[2]} __$${field[0]}`),', ')
+    join(map(scontents,(field) => `$${f_type(field)} __$${f_name(field)}`),', ')
   */
 
   /* $def params
-     map(scontents, (field) => [field[0],`__$${field[0]}`])
+     map(scontents, (field) => [f_name(field),`__$${f_name(field)}`])
   */
 
   function $$(sname)_pack($$(arguments)) internal pure returns (bytes32) {
     return $$(make(
       scontents,
       map(scontents, (field) =>
-    [field[0],`__$${field[0]}`])));
+    [f_name(field),`__$${f_name(field)}`])));
   }
 
   function $$(sname)_unpack(bytes32 __packed) internal pure returns ($$(arguments)) {
     // $for field in scontents
-    __$$(field[0]) = $$(get('__packed',scontents,field[0]));
+    __$$(f_name(field)) = $$(get('__packed',scontents,f_name(field)));
     // $done
   }
 
   // $for field in scontents
-  function $$(sname)_unpack_$$(field[0])(bytes32 __packed) internal pure returns($$(field[2])) {
-    return $$(get('__packed',scontents,field[0]));
+  function $$(sname)_unpack_$$(f_name(field))(bytes32 __packed) internal pure returns($$(f_type(field))) {
+    return $$(get('__packed',scontents,f_name(field)));
   }
   // $done
 
