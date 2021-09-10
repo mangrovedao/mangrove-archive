26972a3b6f8220e6856a6070b9b050caf135eea0
Jean Krivine <Jean.Krivine@irif.fr>
2021-09-10 17:02:37 +0200

    aave-lender strat wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,10 @@ require("hardhat-deploy-ethers");
 require("hardhat-abi-exporter");
 require("adhusson-hardhat-solpp");
 
-require("./lib/hardhat-ethereum-env.js"); // Adds Ethereum environment to Hardhat Runtime Envrionment
-require("./lib/hardhat-polygon-env.js"); // Adds Polygon environment to Hardhat Runtime Envrionment
+require("./lib/hardhat-mainnet-env.js"); // Adds [Ethereum|polygon] mainnet environment to Hardhat Runtime Envrionment
+// FIXME the console approach is not working due to the spawning of a new process
+//require("./lib/mangrove-console.js"); // Add auto-deploy of Mangrove to the Hardhat Console
+//require("./lib/hardhat-mangrove.js");
 
 require("@giry/hardhat-test-solidity");
 // Use Hardhat configuration from loaded configuration files
