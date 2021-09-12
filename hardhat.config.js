3399463bb2e822274f5cb60616b4a950920a29e3
Espen Højsgaard <espen@espen.dk>
2021-09-12 12:53:57 +0200

    Clean up hardhat.config.js and deprecated in-repo hardhat extensions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,16 @@
 //usePlugin("@nomiclabs/buidler-truffle5");
-process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
 require("dotenv-flow").config(); // Reads local environment variables from .env*.local files
+if (!process.env["NODE_CONFIG_DIR"]) {
+    process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
+}
 const config = require("config"); // Reads configuration files from /config/
 require("hardhat-deploy");
 require("hardhat-deploy-ethers");
 require("hardhat-abi-exporter");
 require("adhusson-hardhat-solpp");
-const test_solidity = require("./lib/test_solidity.js");
 
-require("./lib/hardhat-mainnet-env.js"); // Adds [Ethereum|polygon] mainnet environment to Hardhat Runtime Envrionment
-// FIXME the console approach is not working due to the spawning of a new process
-//require("./lib/mangrove-console.js"); // Add auto-deploy of Mangrove to the Hardhat Console
-//require("./lib/hardhat-mangrove.js");
+require("./lib/hardhat-ethereum-env.js"); // Adds Ethereum environment to Hardhat Runtime Envrionment
+require("./lib/hardhat-polygon-env.js"); // Adds Polygon environment to Hardhat Runtime Envrionment
 
 require("@giry/hardhat-test-solidity");
 // Use Hardhat configuration from loaded configuration files
