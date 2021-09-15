1a5eb33b8800dd2c66def588f94d658f029fe3b5
Troels Damgaard <troels@tedconsulting.dk>
2021-09-15 09:02:00 +0200

    bot-bootstrap in own folder

--- packages/bot-bootstrap/src/populate-market.js
+++ packages/bot-bootstrap/src/populate-market.js
@@ -13,7 +13,7 @@ const lc = require("../../../test/libcommon");
  */
 
 // const { Mangrove } = require("../../../mangrove.js/src/index.ts");
-const { Mangrove } = require("../mangrove.js/dist/nodejs/mangrove");
+const { Mangrove } = require("../../../mangrove.js/dist/nodejs/mangrove");
 
 const mnemonic = hre.network.config.accounts.mnemonic;
 const addresses = [];
@@ -41,7 +41,7 @@ const main = async () => {
         }    
         );
 
-    const tokenAbi = require("../build/cache/solpp-generated-contracts/Tests/Agents/TestToken.sol/TestToken.json").abi;
+    const tokenAbi = require("../../../build/cache/solpp-generated-contracts/Tests/Agents/TestToken.sol/TestToken.json").abi;
     const TokenA = new hre.ethers.Contract(mgv.getAddress("TokenA"), tokenAbi, mgv._provider);
     const TokenB = new hre.ethers.Contract(mgv.getAddress("TokenB"), tokenAbi, mgv._provider);
       
