77b28ffbd8d00dfc72bb6c41ace0f2d83f8eafce
Espen Højsgaard <espen@espen.dk>
2021-09-20 15:15:42 +0200

    Remove folders added by mistake

--- mangrove.js/dist/nodejs/types/typechain/index.js
+++
@@ -1,5 +0,0 @@
-export { Mangrove__factory } from "./factories/Mangrove__factory";
-export { IERC20__factory } from "./factories/IERC20__factory";
-export { MgvEvents__factory } from "./factories/MgvEvents__factory";
-export { MgvReader__factory } from "./factories/MgvReader__factory";
-//# sourceMappingURL=index.js.map
