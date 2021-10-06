72e73faa36019093a7c37d285b8674a358019c56
Espen Højsgaard <espen@espen.dk>
2021-10-06 16:33:58 +0200

    Mangrove.js: Move Mocha Context extension to type file

--- packages/cleaning-bot-naive/test/integration/infrastructure.integration.tests.ts
+++ packages/cleaning-bot-naive/test/integration/infrastructure.integration.tests.ts
@@ -7,13 +7,6 @@ const { expect } = chai;
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
 import Mangrove from "@giry/mangrove-js";
-import { Provider } from "@ethersproject/abstract-provider";
-
-declare module "mocha" {
-  export interface Context {
-    provider: Provider;
-  }
-}
 
 describe("Can connect to Mangrove on local chain", () => {
   it("should be able to connect to Mangrove", function () {
