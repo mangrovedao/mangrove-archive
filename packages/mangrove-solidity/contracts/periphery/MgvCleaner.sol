41fc4bd71fbcdbf5d288d8b30bb21b8aec83d3ea
Adrien Husson <adhusson@gmail.com>
2021-10-11 17:37:43 +0200

    Update Cleaner to MgvCleaner, add tests.

---
+++ packages/mangrove-solidity/contracts/periphery/MgvCleaner.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
+import "../Mangrove.sol";
+
+/* The purpose of the Cleaner contract is to execute failing offers and collect
+ * their associated bounty. It takes an array of offers with same definition as
+ * `Mangrove.snipes` and expects them all to fail or not execute. */
+
+/* How to use:
+   1) Ensure *your* address approved Mangrove for the token you will provide to the offer (`inbound_tkn`).
+   2) Run `collect` on the offers that you detected were failing.
+
+   You can adjust takerWants/takerGives and gasreq as needed.
+
+   Note: in the current version you do not need to set MgvCleaner's allowance in Mangrove.
+   TODO: add `collectWith` with an additional `taker` argument.
+*/
+contract MgvCleaner {
+  Mangrove immutable MGV;
+
+  constructor(address payable _MGV) {
+    MGV = Mangrove(_MGV);
+  }
+
+  receive() external payable {}
+
+  function collect(
+    address outbound_tkn,
+    address inbound_tkn,
+    uint[4][] calldata targets,
+    bool fillWants
+  ) external returns (uint bal) {
+    (uint successes, , ) = MGV.snipesFor(
+      outbound_tkn,
+      inbound_tkn,
+      targets,
+      fillWants,
+      msg.sender
+    );
+    console.log("successes", successes);
+    require(successes == 0, "mgvCleaner/anOfferDidNotFail");
+    bal = address(this).balance;
+    msg.sender.send(bal);
+  }
+}
