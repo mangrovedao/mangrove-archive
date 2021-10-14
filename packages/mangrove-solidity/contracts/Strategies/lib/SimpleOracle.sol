a997f32fb78deb86d7bb2aa85eb75d02cf3b3ffb
Jean Krivine <jean.krivine@irif.fr>
2021-10-14 17:33:44 +0200

    IOracle should provide decimals

--- packages/mangrove-solidity/contracts/Strategies/lib/SimpleOracle.sol
+++ packages/mangrove-solidity/contracts/Strategies/lib/SimpleOracle.sol
@@ -4,15 +4,29 @@ pragma abicoder v2;
 
 import "../interfaces/IOracle.sol";
 import "./AccessControlled.sol";
+import {IERC20} from "../../MgvLib.sol";
 
 contract SimpleOracle is IOracle, AccessControlled {
   address reader; // if unset, anyone can read price
+  IERC20 public immutable base_token;
+  mapping(address => uint96) internal priceData;
+
+  constructor(address _base) {
+    try IERC20(_base).decimals() returns (uint8 d) {
+      require(d != 0, "Invalid decimals number for Oracle base");
+      base_token = IERC20(_base);
+    } catch {
+      revert("Invalid Oracle base address");
+    }
+  }
+
+  function decimals() external view override returns (uint8) {
+    return base_token.decimals();
+  }
 
   function setReader(address _reader) external onlyAdmin {
     reader = _reader;
   }
-
-  mapping(address => uint96) internal priceData;
 
   function setPrice(address token, uint price) external override onlyAdmin {
     require(uint96(price) == price, "price overflow");
