ad173b2baf71e63cb89a7bc5c368b40247fc04cc
Espen Højsgaard <espen@espen.dk>
2021-10-14 20:11:02 +0200

    Cleaning bot: Integration tests on both asks and bids

--- packages/cleaning-bot/test/integration/marketCleaner.integration.tests.ts
+++ packages/cleaning-bot/test/integration/marketCleaner.integration.tests.ts
@@ -13,6 +13,9 @@ import { Provider } from "@ethersproject/abstract-provider";
 import { MarketCleaner } from "../../src/MarketCleaner";
 import * as hre from "hardhat";
 import "hardhat-deploy-ethers/dist/src/type-extensions";
+import { BookSide } from "../../src/mangrove-js-type-aliases";
+
+const bookSides: BookSide[] = ["asks", "bids"];
 
 describe("MarketCleaner integration tests", () => {
   let provider: Provider;
@@ -40,68 +43,53 @@ describe("MarketCleaner integration tests", () => {
     mgv.disconnect();
   });
 
-  it("should clean offer failing to trade 0 wants on the 'asks' offer list", async function () {
-    // Arrange
-    // - Set up accounts
-    //   - Which do we need?
-    //     - MGV admin account for opening market
-    //     - Maker account for creating offer
-    //       - Could perhaps be the same as MGV admin?
-    //     - Cleaner account for sending transactions
-    //   - What funding do the accounts need?
-    //     - Plenty of ether for gas - we're not testing out of gas here
-    //     - Only Maker acct needs tokens
-    //   - Do we need to set up any token approvals?
-    //     - For ERC-20 tokens, I think both Maker acct and Cleaner acct need to be approved?
-    //     - NB: MgvToken has utility function for approving
-    //   - Maker must be provisioned
-    // - Open market
-    //   - NB: Market (TokenA, TokenB) is already opened by the integration-test-root-hooks
-    //   - TODO which account - deployer?
-    //   - NB: There's no Mangrove.js API for opening markets, right?
-    console.log((await market.config()).asks.active);
-    // - Add offer to order book - using Mangrove.js if possible
-    //   - Must be failing
-    //   - Must not be persistent
-    //   - Cleaning it must be profitable
-    // TODO which account is used for this transaction - deployer, right?
-    const setShouldFailTx = await testMakerContract.shouldFail(true);
-    await setShouldFailTx.wait();
-    // console.log("Calling TestMaker.provisionMgv(...)");
-    // const provisionTx = await testMakerContract.provisionMgv(toWei("1"));
-    // // console.dir(provisionTx);
-    // const provisionTxReceipt = await provisionTx.wait();
-    // // console.dir(provisionTxReceipt);
-    const newOfferTx = await testMakerContract[
-      "newOffer(address,address,uint256,uint256,uint256,uint256)"
-    ](market.base.address, market.quote.address, 1, 1000000, 100, 1);
-    // console.dir(newOfferTx);
-    const newOfferTxReceipt = await newOfferTx.wait();
-    // console.dir(newOfferTxReceipt);
-    // const newOfferTx = await newOffer(mgv, market.base, market.quote, {
-    //   wants: "1",
-    //   gives: "1.2",
-    //   gasreq: 10000,
-    //   gasprice: 1,
-    // });
-    // console.dir(newOfferTx);
-    // const newOfferTxReceipt = await newOfferTx.wait();
-    // console.dir(newOfferTxReceipt);
+  bookSides.forEach((bookSide) => {
+    it(`should clean offer failing to trade 0 wants on the '${bookSide}' offer list`, async function () {
+      // Arrange
+      // - Set up accounts
+      //   - Which do we need?
+      //     - MGV admin account for opening market
+      //     - Maker account for creating offer
+      //       - Could perhaps be the same as MGV admin?
+      //     - Cleaner account for sending transactions
+      //   - What funding do the accounts need?
+      //     - Plenty of ether for gas - we're not testing out of gas here
+      //     - Only Maker acct needs tokens
+      //   - Do we need to set up any token approvals?
+      //     - For ERC-20 tokens, I think both Maker acct and Cleaner acct need to be approved?
+      //     - NB: MgvToken has utility function for approving
+      //   - Maker must be provisioned
+      // - Open market
+      //   - NB: Market (TokenA, TokenB) is already opened by the integration-test-root-hooks
+      //   - TODO which account - deployer?
+      //   - NB: There's no Mangrove.js API for opening markets, right?
+      // - Add offer to order book - using Mangrove.js if possible
+      //   - Must be failing
+      //   - Must not be persistent
+      //   - Cleaning it must be profitable
+      // TODO which account is used for this transaction - deployer, right?
+      const setShouldFailTx = await testMakerContract.shouldFail(true);
+      await setShouldFailTx.wait();
+      const newOfferTx = await testMakerContract[
+        "newOffer(address,address,uint256,uint256,uint256,uint256)"
+      ](market.base.address, market.quote.address, 1, 1000000, 100, 1);
+      const newOfferTxReceipt = await newOfferTx.wait();
 
-    // Act
-    // - Create MarketCleaner for the market
-    //   - Must use the right account
-    // - Wait for it to complete cleaning - HOW?
-    const marketCleaner = new MarketCleaner(market, provider);
-    await marketCleaner.clean(0);
+      // Act
+      // - Create MarketCleaner for the market
+      //   - Must use the right account
+      // - Wait for it to complete cleaning - HOW?
+      const marketCleaner = new MarketCleaner(market, provider);
+      await marketCleaner.clean(0);
 
-    // Assert
-    return Promise.all([
-      // - Offer is not in the 'asks' offer list
-      expect(market.requestBook()).to.eventually.have.property("asks").which.is
-        .empty,
-      // - Cleaner acct has more ether than before
-      //   - Can we calculate exactly what the balance should be?
-    ]);
+      // Assert
+      return Promise.all([
+        // - Offer is not in the 'bookSide' offer list
+        expect(market.requestBook()).to.eventually.have.property(bookSide).which
+          .is.empty,
+        // - Cleaner acct has more ether than before
+        //   - Can we calculate exactly what the balance should be?
+      ]);
+    });
   });
 });
