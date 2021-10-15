5af2aa89b72cbae870885e6f9e4503e9bae68e18
Jean Krivine <jean.krivine@irif.fr>
2021-10-15 17:56:35 +0200

    Non reverting execute (preparing for the new spec)

--- packages/mangrove-solidity/contracts/Strategies/OfferMakerStrats/Defensive.sol
+++ packages/mangrove-solidity/contracts/Strategies/OfferMakerStrats/Defensive.sol
@@ -12,6 +12,9 @@ abstract contract Defensive is MangroveOffer {
   uint16 constant slippage_den = 10**4;
   IOracle public oracle;
 
+  // emitted when no price data is available for given token
+  event MissingPrice(address token);
+
   constructor(address _oracle) {
     require(!(_oracle == address(0)), "Invalid oracle address");
     oracle = IOracle(_oracle);
@@ -27,7 +30,7 @@ abstract contract Defensive is MangroveOffer {
     internal
     virtual
     override
-    returns (bool proceed)
+    returns (bool)
   {
     uint offer_gives_REF = mul_(
       order.wants,
@@ -38,12 +41,13 @@ abstract contract Defensive is MangroveOffer {
       oracle.getPrice(order.inbound_tkn) // returns price is oracle base units (i.e ETH or USD)
     );
     // abort trade if price data is not available
-    if (offer_gives_REF == 0 || offer_wants_REF == 0) {
-      returnData({
-        drop: true,
-        postHook_switch: PostHook.Fallback,
-        message: "Missing price data"
-      });
+    if (offer_gives_REF == 0) {
+      emit MissingPrice(order.outbound_tkn);
+      return false;
+    }
+    if (offer_wants_REF == 0) {
+      emit MissingPrice(order.inbound_tkn);
+      return false;
     }
     // if offer_gives_REF * (1-slippage) > offer_wants_REF one is getting arb'ed
     // i.e slippage_den * OGR - slippage_num * OGR > OWR * slippage_den
