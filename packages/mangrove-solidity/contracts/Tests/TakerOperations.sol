278dd56c36c12aab7dec294ca77523b267e90613
Adrien Husson <adhusson@gmail.com>
2021-10-15 17:25:01 +0200

    Update offer filling logic, see description
    Formerly if `offerWants=0`, a taker with `fillWants=true` would still
    receive more than they asked for due to the `|| offerWants == 0` part of
    the modified `if`.
    
    This does not respect the intended spec of `fillWants` which is to give
    at most `takerWants` to the taker.
    
    We remove the disjunct from the conditional and update the case where
    `fillWants=false` to avoid division by 0.

--- packages/mangrove-solidity/contracts/Tests/TakerOperations.sol
+++ packages/mangrove-solidity/contracts/Tests/TakerOperations.sol
@@ -201,6 +201,70 @@ contract TakerOperations_Test is HasMgvEvents {
     }
   }
 
+  function snipe_free_offer_fillWants_respects_spec_test() public {
+    uint ofr = mkr.newOffer(0, 1 ether, 100_000, 0);
+    TestEvents.check(
+      TestUtils.hasOffer(mgv, base, quote, ofr),
+      "Offer should be in the book"
+    );
+    mkr.expect("mgv/tradeSuccess"); // trade should be OK on the maker side
+    quoteT.approve(address(mgv), 1 ether);
+
+    /* Setting fillWants = true means we should not receive more than `wants`.
+       Here we are asking for 0.1 eth to an offer that gives 1eth for nothing.
+       We should still only receive 0.1 eth */
+
+    uint[4][] memory targets = new uint[4][](1);
+    targets[0] = [ofr, 0.1 ether, 0, 100_000];
+    try mgv.snipes(base, quote, targets, true) returns (
+      uint successes,
+      uint got,
+      uint gave
+    ) {
+      TestEvents.check(successes == 1, "Snipe should not fail");
+      TestEvents.eq(got, 0.1 ether, "Wrong got value");
+      TestEvents.eq(gave, 0 ether, "Wrong gave value");
+      TestEvents.check(
+        !TestUtils.hasOffer(mgv, base, quote, ofr),
+        "Offer should not be in the book"
+      );
+    } catch {
+      TestEvents.fail("Transaction should not revert");
+    }
+  }
+
+  function snipe_free_offer_fillGives_respects_spec_test() public {
+    uint ofr = mkr.newOffer(0, 1 ether, 100_000, 0);
+    TestEvents.check(
+      TestUtils.hasOffer(mgv, base, quote, ofr),
+      "Offer should be in the book"
+    );
+    mkr.expect("mgv/tradeSuccess"); // trade should be OK on the maker side
+    quoteT.approve(address(mgv), 1 ether);
+
+    /* Setting fillWants = false means we should spend as little as possible to receive
+       as much as possible.
+       Here despite asking for .1eth the offer gives 1eth for 0 so we should receive 1eth. */
+
+    uint[4][] memory targets = new uint[4][](1);
+    targets[0] = [ofr, 0.1 ether, 0, 100_000];
+    try mgv.snipes(base, quote, targets, false) returns (
+      uint successes,
+      uint got,
+      uint gave
+    ) {
+      TestEvents.check(successes == 1, "Snipe should not fail");
+      TestEvents.eq(got, 1 ether, "Wrong got value");
+      TestEvents.eq(gave, 0 ether, "Wrong gave value");
+      TestEvents.check(
+        !TestUtils.hasOffer(mgv, base, quote, ofr),
+        "Offer should not be in the book"
+      );
+    } catch {
+      TestEvents.fail("Transaction should not revert");
+    }
+  }
+
   function snipe_fillGives_zero_test() public {
     uint ofr = mkr.newOffer(1 ether, 1 ether, 100_000, 0);
     TestEvents.check(
