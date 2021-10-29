bac77b1e5522db8ef82d83d1238599f1e8c54cc1
Espen Højsgaard <espen@espen.dk>
2021-10-29 09:28:03 +0200

    cleaning-bot: Extract test utility functions for readability and reuse

--- packages/cleaning-bot/test/integration/marketCleaner.integration.tests.ts
+++ packages/cleaning-bot/test/integration/marketCleaner.integration.tests.ts
@@ -7,258 +7,85 @@ const { expect } = chai;
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
 
-import { Mangrove, Market, MgvToken } from "@giry/mangrove-js";
-import * as typechain from "@giry/mangrove-js/dist/nodejs/types/typechain";
-import { BookSide } from "../../src/mangrove-js-type-aliases";
+import { Mangrove, Market } from "@giry/mangrove-js";
 import { ethers } from "ethers";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
-import { ethers as hardhatEthers } from "hardhat";
 import { Provider } from "@ethersproject/abstract-provider";
-import { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
 import { MarketCleaner } from "../../dist/nodejs/MarketCleaner";
-import { toWei } from "../util/helpers";
+import {
+  Account,
+  Balances,
+  bookSides,
+  getAccount,
+  logAddresses,
+  postNewSucceedingOffer,
+  postNewRevertingOffer,
+  getBalances,
+  logBalances,
+  setMgvGasPrice,
+  mint,
+  approveMgv,
+  AccountName,
+} from "../util/helpers";
 
-const bookSides: BookSide[] = ["asks", "bids"];
+let maker: Account; // Owner of TestMaker contract
+let cleaner: Account; // Owner of cleaner EOA
+let accounts: Account[]; // All referenced accounts for easy debugging
 
-type AccountBalances = {
-  ether: ethers.BigNumber;
-  tokenA: ethers.BigNumber;
-  tokenB: ethers.BigNumber;
-};
+let balancesBefore: Map<string, Balances>; // account name |-> balances
 
-type Account = {
-  name: string;
-  address: string;
-  signer: SignerWithAddress;
-  contracts: {
-    // Contracts connected with the signer for setting chain state in test case setup
-    mangrove: typechain.Mangrove;
-    testMaker: typechain.TestMaker;
-    tokenA: typechain.TestTokenWithDecimals;
-    tokenB: typechain.TestTokenWithDecimals;
-  };
-};
+let testProvider: Provider; // Only used to read state for assertions, not associated with an account
+let cleanerProvider: Provider; // Tied to the cleaner bot's account
 
-type Addresses = {
-  mangrove: string;
-  testMaker: string;
-  tokenA: string;
-  tokenB: string;
-};
-
-type TestOffer = {
-  bookSide: BookSide;
-  wants: ethers.BigNumberish;
-  gives: ethers.BigNumberish;
-  gasreq?: ethers.BigNumberish;
-  shouldFail?: boolean;
-  shouldAbort?: boolean;
-  shouldRevert?: boolean;
-};
+let mgv: Mangrove;
+let market: Market;
 
 describe("MarketCleaner integration tests", () => {
-  let deployer: Account; // Owner of deployed MGV and token contracts
-  let maker: Account; // Owner of TestMaker contract
-  let cleaner: Account; // Owner of cleaner EOA
-  let accounts: Account[]; // All referenced accounts for easy debugging
-
-  let balancesBefore: Map<string, AccountBalances>; // account name |-> balances
-  let balancesAfter: Map<string, AccountBalances>; // account name |-> balances
-
-  let testProvider: Provider; // Only used to read state for assertions, not associated with an account
-  let cleanerProvider: Provider; // Tied to the cleaner bot's account
-
-  let mgv: Mangrove;
-  let market: Market;
-
-  let addresses: Addresses;
-
-  const initAddresses = async () => {
-    addresses = {
-      mangrove: (await hardhatEthers.getContract("Mangrove")).address,
-      testMaker: (await hardhatEthers.getContract("TestMaker")).address,
-      tokenA: (await hardhatEthers.getContract("TokenA")).address,
-      tokenB: (await hardhatEthers.getContract("TokenB")).address,
-    };
-  };
-
-  const logAddresses = () => {
-    console.group("Addresses");
-    Object.entries(addresses).map(([key, value]) =>
-      console.log(`${key}: ${value}`)
-    );
-    console.groupEnd();
-  };
-
-  const initContracts = async (signer: ethers.Signer) => {
-    return {
-      mangrove: typechain.Mangrove__factory.connect(addresses.mangrove, signer),
-      testMaker: typechain.TestMaker__factory.connect(
-        addresses.testMaker,
-        signer
-      ),
-      tokenA: typechain.TestTokenWithDecimals__factory.connect(
-        addresses.tokenA,
-        signer
-      ),
-      tokenB: typechain.TestTokenWithDecimals__factory.connect(
-        addresses.tokenB,
-        signer
-      ),
-    };
-  };
-
-  const initAccount = async (name: string) => {
-    const signer = await hardhatEthers.getNamedSigner(name);
-    const account = {
-      name: name,
-      address: signer.address,
-      signer: signer,
-      contracts: await initContracts(signer),
-    };
-    return account;
-  };
-
-  const initAccounts = async () => {
-    deployer = await initAccount("deployer");
-    maker = await initAccount("maker");
-    cleaner = await initAccount("cleaner");
-
-    accounts = [deployer, maker, cleaner];
-  };
-
-  const getAccountBalances = async (
-    account: Account
-  ): Promise<AccountBalances> => {
-    return {
-      ether: await testProvider.getBalance(account.address),
-      tokenA: await account.contracts.tokenA.balanceOf(account.address),
-      tokenB: await account.contracts.tokenB.balanceOf(account.address),
-    };
-  };
-
-  const getBalances = async () => {
-    const balances = new Map<string, AccountBalances>();
-    for (const account of accounts) {
-      balances.set(account.name, await getAccountBalances(account));
-    }
-    return balances;
-  };
-
-  const getTokens = (bookSide: BookSide) => {
-    return {
-      inboundToken: bookSide === "asks" ? market.base : market.quote,
-      outboundToken: bookSide === "asks" ? market.quote : market.base,
-    };
-  };
-
-  const newOffer = async ({
-    bookSide,
-    wants,
-    gives,
-    gasreq = 5e4,
-    shouldFail = false,
-    shouldAbort = false,
-    shouldRevert = false,
-  }: TestOffer) => {
-    const { inboundToken, outboundToken } = getTokens(bookSide);
-
-    await maker.contracts.testMaker
-      .shouldFail(shouldFail)
-      .then((tx) => tx.wait());
-    await maker.contracts.testMaker
-      .shouldAbort(shouldAbort)
-      .then((tx) => tx.wait());
-    await maker.contracts.testMaker
-      .shouldRevert(shouldRevert)
-      .then((tx) => tx.wait());
-
-    await maker.contracts.testMaker[
-      "newOffer(address,address,uint256,uint256,uint256,uint256)"
-    ](inboundToken.address, outboundToken.address, wants, gives, gasreq, 1) // (base address, quote address, wants, gives, gasreq, pivotId)
-      .then((tx) => tx.wait());
-  };
-
-  const newRevertingOffer = async (bookSide: BookSide) => {
-    await newOffer({
-      bookSide: bookSide,
-      wants: 1,
-      gives: 1000000,
-      shouldRevert: true,
-    });
-  };
-
-  const newSucceedingOffer = async (bookSide: BookSide) => {
-    await newOffer({
-      bookSide,
-      wants: 1,
-      gives: 1000000,
-    });
-  };
-
   before(async function () {
-    await initAddresses();
-  });
-
-  beforeEach(async function () {
     testProvider = new ethers.providers.JsonRpcProvider(
       this.test?.parent?.parent?.ctx.providerUrl
     );
+  });
 
-    await initAccounts();
+  beforeEach(async function () {
+    maker = await getAccount(AccountName.Maker);
+    cleaner = await getAccount(AccountName.Cleaner);
 
-    // Turn up the Mangrove gasprice to increase the bounty
-    await deployer.contracts.mangrove.setGasprice(50).then((tx) => tx.wait());
-
-    await deployer.contracts.tokenA
-      .mint(maker.address, toWei(10))
-      .then((tx) => tx.wait());
-    await deployer.contracts.tokenB
-      .mint(maker.address, toWei(10))
-      .then((tx) => tx.wait());
-
-    await maker.contracts.tokenA
-      .approve(addresses.mangrove, toWei(100))
-      .then((tx) => tx.wait());
-    await maker.contracts.tokenB
-      .approve(addresses.mangrove, toWei(100))
-      .then((tx) => tx.wait());
-
-    balancesBefore = await getBalances();
+    accounts = [maker, cleaner];
 
     mgv = await Mangrove.connect({
       provider: this.test?.parent?.parent?.ctx.providerUrl,
       signer: cleaner.signer,
     });
-    cleanerProvider = mgv._provider;
     market = await mgv.market({ base: "TokenA", quote: "TokenB" });
+
+    cleanerProvider = mgv._provider;
+
+    // Turn up the Mangrove gasprice to increase the bounty
+    await setMgvGasPrice(50);
+    await mint(market.base, maker, 10);
+    await mint(market.quote, maker, 10);
+
+    await approveMgv(market.base, maker, 100);
+    await approveMgv(market.quote, maker, 100);
+
+    balancesBefore = await getBalances(accounts, testProvider);
   });
 
   afterEach(async function () {
     market.disconnect();
     mgv.disconnect();
 
-    balancesAfter = await getBalances();
-
-    for (const account of accounts) {
-      console.group(`${account.name} balances`);
-      console.group("Before");
-      console.dir(balancesBefore.get(account.name));
-      console.groupEnd();
-      console.group("After");
-      console.dir(balancesAfter.get(account.name));
-      console.groupEnd();
-      console.groupEnd();
-    }
-
+    const balancesAfter = await getBalances(accounts, testProvider);
+    logBalances(accounts, balancesBefore, balancesAfter);
     logAddresses();
   });
 
   bookSides.forEach((bookSide) => {
     it(`should clean offer failing to trade 0 wants on the '${bookSide}' offer list`, async function () {
       // Arrange
-      await newRevertingOffer(bookSide);
+      await postNewRevertingOffer(market, bookSide, maker);
 
       const marketCleaner = new MarketCleaner(market, cleanerProvider);
 
@@ -278,7 +105,7 @@ describe("MarketCleaner integration tests", () => {
 
     it(`should not clean offer suceeding to trade 0 wants on the '${bookSide}' offer list`, async function () {
       // Arrange
-      await newSucceedingOffer(bookSide);
+      await postNewSucceedingOffer(market, bookSide, maker);
 
       const marketCleaner = new MarketCleaner(market, cleanerProvider);
 
--- packages/cleaning-bot/test/util/helpers.ts
+++ packages/cleaning-bot/test/util/helpers.ts
@@ -1,32 +1,270 @@
 // FIXME this is a TypeScriptified excerpt of the file Mangrove-js/test/util/helper.js - we should find a better way...
-import { BigNumberish, utils } from "ethers";
+import { BigNumber, BigNumberish, utils } from "ethers";
 
-import { Mangrove, MgvToken } from "@giry/mangrove-js";
+import { Market, MgvToken } from "@giry/mangrove-js";
+import { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
+import * as typechain from "@giry/mangrove-js/dist/nodejs/types/typechain";
+import { BookSide } from "../../src/mangrove-js-type-aliases";
+import { ethers } from "ethers";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import { ethers as hardhatEthers } from "hardhat";
+import { Provider } from "@ethersproject/abstract-provider";
 
-export const toWei = (v: number, u = "ether") =>
+export const toWei = (v: number, u = "ether"): BigNumber =>
   utils.parseUnits(v.toString(), u);
 
-export interface EoaOfferSpec {
-  wants: string;
-  gives: string;
-  gasreq: BigNumberish;
-  gasprice: BigNumberish;
-}
+export type Account = {
+  name: string;
+  address: string;
+  signer: SignerWithAddress;
+  connectedContracts: {
+    // Contracts connected with the signer for setting chain state in test case setup
+    mangrove: typechain.Mangrove;
+    testMaker: typechain.TestMaker;
+    tokenA: typechain.TestTokenWithDecimals;
+    tokenB: typechain.TestTokenWithDecimals;
+  };
+};
 
-// Creates a new offer referring to an EOA instead of a Maker contract
-export const newOffer = (
-  mgv: Mangrove,
-  base: MgvToken,
-  quote: MgvToken,
-  { wants, gives, gasreq = 10_000, gasprice = 1 }: EoaOfferSpec
-) => {
-  return mgv.contract.newOffer(
-    base.address,
-    quote.address,
-    exports.toWei(wants),
-    exports.toWei(gives),
-    gasreq,
-    gasprice,
-    0
+export type Balances = {
+  ether: ethers.BigNumber;
+  tokenA: ethers.BigNumber;
+  tokenB: ethers.BigNumber;
+};
+
+export const bookSides: BookSide[] = ["asks", "bids"];
+
+export type Addresses = {
+  mangrove: string;
+  testMaker: string;
+  tokenA: string;
+  tokenB: string;
+};
+
+let addresses: Addresses;
+
+export const getAddresses = async (): Promise<Addresses> => {
+  if (!addresses) {
+    addresses = {
+      mangrove: (await hardhatEthers.getContract("Mangrove")).address,
+      testMaker: (await hardhatEthers.getContract("TestMaker")).address,
+      tokenA: (await hardhatEthers.getContract("TokenA")).address,
+      tokenB: (await hardhatEthers.getContract("TokenB")).address,
+    };
+  }
+  return addresses;
+};
+
+export const logAddresses = async (): Promise<void> => {
+  console.group("Addresses");
+  Object.entries(await getAddresses()).map(([key, value]) =>
+    console.log(`${key}: ${value}`)
   );
+  console.groupEnd();
+};
+
+export type Contracts = {
+  mangrove: typechain.Mangrove;
+  testMaker: typechain.TestMaker;
+  tokenA: typechain.TestTokenWithDecimals;
+  tokenB: typechain.TestTokenWithDecimals;
+};
+
+export const getContracts = async (
+  signer: ethers.Signer
+): Promise<Contracts> => {
+  const addresses = await getAddresses();
+  return {
+    mangrove: typechain.Mangrove__factory.connect(addresses.mangrove, signer),
+    testMaker: typechain.TestMaker__factory.connect(
+      addresses.testMaker,
+      signer
+    ),
+    tokenA: typechain.TestTokenWithDecimals__factory.connect(
+      addresses.tokenA,
+      signer
+    ),
+    tokenB: typechain.TestTokenWithDecimals__factory.connect(
+      addresses.tokenB,
+      signer
+    ),
+  };
+};
+
+export enum AccountName {
+  Deployer = "deployer", // Owner of deployed MGV and token contracts
+  Maker = "maker", // Owner of TestMaker contract
+  Cleaner = "cleaner", // Owner of cleaner EOA
+}
+const accounts = new Map<AccountName, Account>();
+
+export const getAccount = async (name: AccountName): Promise<Account> => {
+  let account = accounts.get(name);
+  if (!account) {
+    const signer = await hardhatEthers.getNamedSigner(name);
+    account = {
+      name: name,
+      address: signer.address,
+      signer: signer,
+      connectedContracts: await getContracts(signer),
+    };
+    accounts.set(name, account);
+  }
+  return account;
+};
+
+export const getAccountBalances = async (
+  account: Account,
+  provider: Provider
+): Promise<Balances> => {
+  return {
+    ether: await provider.getBalance(account.address),
+    tokenA: await account.connectedContracts.tokenA.balanceOf(account.address),
+    tokenB: await account.connectedContracts.tokenB.balanceOf(account.address),
+  };
+};
+
+export const getBalances = async (
+  accounts: Account[],
+  provider: Provider
+): Promise<Map<string, Balances>> => {
+  const balances = new Map<string, Balances>();
+  for (const account of accounts) {
+    balances.set(account.name, await getAccountBalances(account, provider));
+  }
+  return balances;
+};
+
+export const logBalances = async (
+  accounts: Account[],
+  balancesBefore: Map<string, Balances>,
+  balancesAfter: Map<string, Balances>
+): Promise<void> => {
+  for (const account of accounts) {
+    console.group(`${account.name} balances`);
+    console.group("Before");
+    console.dir(balancesBefore.get(account.name));
+    console.groupEnd();
+    console.group("After");
+    console.dir(balancesAfter.get(account.name));
+    console.groupEnd();
+    console.groupEnd();
+  }
+};
+
+export const getTokens = (
+  market: Market,
+  bookSide: BookSide
+): {
+  inboundToken: MgvToken;
+  outboundToken: MgvToken;
+} => {
+  return {
+    inboundToken: bookSide === "asks" ? market.base : market.quote,
+    outboundToken: bookSide === "asks" ? market.quote : market.base,
+  };
+};
+
+export type NewOffer = {
+  market: Market;
+  bookSide: BookSide;
+  maker: Account;
+  wants?: ethers.BigNumberish;
+  gives?: ethers.BigNumberish;
+  gasreq?: ethers.BigNumberish;
+  shouldFail?: boolean;
+  shouldAbort?: boolean;
+  shouldRevert?: boolean;
+};
+
+// By default, a new offer will succeed
+export const postNewOffer = async ({
+  market,
+  bookSide,
+  maker,
+  wants = 1,
+  gives = 1000000,
+  gasreq = 5e4,
+  shouldFail = false,
+  shouldAbort = false,
+  shouldRevert = false,
+}: NewOffer): Promise<void> => {
+  const { inboundToken, outboundToken } = getTokens(market, bookSide);
+
+  await maker.connectedContracts.testMaker
+    .shouldFail(shouldFail)
+    .then((tx) => tx.wait());
+  await maker.connectedContracts.testMaker
+    .shouldAbort(shouldAbort)
+    .then((tx) => tx.wait());
+  await maker.connectedContracts.testMaker
+    .shouldRevert(shouldRevert)
+    .then((tx) => tx.wait());
+
+  await maker.connectedContracts.testMaker[
+    "newOffer(address,address,uint256,uint256,uint256,uint256)"
+  ](inboundToken.address, outboundToken.address, wants, gives, gasreq, 1) // (base address, quote address, wants, gives, gasreq, pivotId)
+    .then((tx) => tx.wait());
+};
+
+export const postNewRevertingOffer = async (
+  market: Market,
+  bookSide: BookSide,
+  maker: Account
+): Promise<void> => {
+  await postNewOffer({
+    market,
+    bookSide,
+    maker,
+    wants: 1,
+    gives: 1000000,
+    shouldRevert: true,
+  });
+};
+
+export const postNewSucceedingOffer = async (
+  market: Market,
+  bookSide: BookSide,
+  maker: Account
+): Promise<void> => {
+  await postNewOffer({ market, bookSide, maker });
+};
+
+export const setMgvGasPrice = async (gasPrice: BigNumberish): Promise<void> => {
+  const deployer = await getAccount(AccountName.Deployer);
+  await deployer.connectedContracts.mangrove
+    .setGasprice(gasPrice)
+    .then((tx) => tx.wait());
+};
+
+export const mint = async (
+  token: MgvToken,
+  receiver: Account,
+  amount: number
+): Promise<void> => {
+  const deployer = await getAccount(AccountName.Deployer);
+  switch (token.name) {
+    case "TokenA":
+      await deployer.connectedContracts.tokenA
+        .mint(receiver.address, toWei(amount))
+        .then((tx) => tx.wait());
+
+      break;
+
+    case "TokenB":
+      await deployer.connectedContracts.tokenB
+        .mint(receiver.address, toWei(amount))
+        .then((tx) => tx.wait());
+
+      break;
+  }
+};
+
+export const approveMgv = async (
+  token: MgvToken,
+  spender: Account,
+  amount: number
+): Promise<void> => {
+  await token.approveMgv(amount).then((tx) => tx.wait());
 };
